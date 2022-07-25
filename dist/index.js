2ea744c82bce2c20c891556144fea16139652e18
r-e-d-ant <mugishathierry65@gmail.com>
2022-07-25 18:12:34 +0200

    basic types

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// -------- Basic Types -------- //
+let id = 5;
+let company = "Tito Muto";
+let isPublished = true;
+let x = 'Hello'; // any datatype
+// initialize
+let age;
+age = 20;
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+
+// -------- Basic Types -------- //
+let id: number = 5;
+let company: string = "Tito Muto";
+let isPublished: boolean = true;
+let x: any = 'Hello';  // any datatype
+
+// initialize
+let age: number;
+age = 20;
+
