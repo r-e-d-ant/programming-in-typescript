69d4cf5dd15bbf8b69a72ccf0dc0d78aa533fc4e
r-e-d-ant <mugishathierry65@gmail.com>
2022-07-25 18:14:11 +0200

    arrays

--- dist/index.js
+++ dist/index.js
@@ -7,3 +7,11 @@ let x = 'Hello'; // any datatype
 // initialize
 let age;
 age = 20;
+// -------- Array -------- //
+let ids = [1, 2, 3, 4, 5];
+let xArr = [1, 2.3, 'hello', true, false];
+// ids.push('Hello'); // will not work because it supposed to contain only numbers.
+ids.push(2002);
+for (var i = 0; i < ids.length; i++) {
+    console.log(ids[i]);
+}
--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,13 @@ let x: any = 'Hello';  // any datatype
 let age: number;
 age = 20;
 
+// -------- Array -------- //
+let ids: number[] = [1,2,3,4,5];
+let xArr: any[] = [1, 2.3, 'hello', true, false];
+
+// ids.push('Hello'); // will not work because it supposed to contain only numbers.
+ids.push(2002);
+
+for(var i: number = 0; i < ids.length; i++) {
+    console.log(ids[i]);
+}
